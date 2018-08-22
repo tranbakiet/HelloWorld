49e97e53adf082f47548b811766e8e2ac070f4df
unknown <206love193@gmail.com>
2018-08-22 16:52:56 +0700

    sinh vien

---
+++ src/kiet/dev/SinhVien.java
@@ -0,0 +1,17 @@
+package kiet.dev;
+
+public class SinhVien {
+	String name ;
+	int tuoi ;
+	String Masv;
+	
+	
+void thi() {
+
+	System.out.println(name +" " + tuoi +" tuoi " +" thi ket thuc hoc phan" );
+    }
+void donghocphi () {
+	
+	System.out.println("Sinh vien da dong hoc phi");
+    }
+}
---
+++ src/kiet/dev/Sinhvientest.java
@@ -0,0 +1,12 @@
+package kiet.dev;
+
+public class Sinhvientest {
+	public static void main(String[] agrs) {
+		SinhVien sinhvien = new SinhVien();
+		sinhvien.name = "Tran Ba Kiet";
+	    sinhvien.tuoi = 21;			
+		sinhvien.thi();
+		sinhvien.donghocphi();
+	}
+
+}
